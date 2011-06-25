80a4c8d311d8967514f1fbf6673c315cf8deb6f8
Marco Franssen <marco.franssen@gmail.com>
2011-06-25 16:34:53 +0200

    Renamed first workshop

---
+++ src/Demos/WorkshopWinForms/MyFilter.cs
@@ -0,0 +1,34 @@
+﻿using Kinect.Core;
+using Kinect.Core.Eventing;
+
+namespace Kinect.Workshop
+{
+    public class MyFilter : Filter<IUserChangedEvent>
+    {
+        public override string Name
+        {
+            get { return "FilterPipe"; }
+        }
+
+        public override void Process(IUserChangedEvent evt)
+        {
+            bool continueProcess = false;
+            //TODO: Workshop -> Part 3:
+            //TODO: Workshop -> Filter hier de frames uit die niet nodig zijn
+
+            //Als de data goed is en je wilt dat de filter door gaat naar de volgende stap
+            if (continueProcess)
+            {
+                //Ga door naar de volgende stap van deze pipe
+                base.Process(evt);
+            }
+            else
+            {
+                //Data is gefilterd.
+                //Als er mensen geabonneerd zijn op het FilteredEvent
+                //laat ze dan weten dat deze data gefilterd is
+                OnFilteredEvent(new MyFilterEventArgs("Data is gefilterd"));
+            }
+        }
+    }
+}
---
+++ src/Demos/WorkshopWinForms/MyFilterEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using Kinect.Core.Eventing;
+
+namespace Kinect.Workshop
+{
+    public class MyFilterEventArgs : FilterEventArgs
+    {
+        public MyFilterEventArgs(string message)
+        {
+            //TODO: Deze kun je uitbreiden met wat extra data indien nodig
+            Message = message;
+        }
+
+        public string Message { get; private set; }
+
+        public override string Name
+        {
+            get { return "MyFilterEventArgs"; }
+        }
+    }
+}
