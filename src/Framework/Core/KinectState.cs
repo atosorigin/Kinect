cf55b8d069db236151f9bc572c390be72cf7c0e1
Marco Franssen <marco.franssen@gmail.com>
2011-06-26 15:35:16 +0200

    Replaced KinectState enum to seperate file

---
+++ src/Framework/Core/KinectState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Kinect.Core
+{
+    /// <summary>
+    /// Kinect states
+    /// </summary>
+    [Flags]
+    public enum KinectState
+    {
+        /// <summary>
+        /// Kinect context is open
+        /// </summary>
+        ContextOpen,
+
+        /// <summary>
+        /// Kinect is initializing
+        /// </summary>
+        Initializing,
+
+        /// <summary>
+        /// Kinect has failed
+        /// </summary>
+        Failed,
+
+        /// <summary>
+        /// Kinect is stopped
+        /// </summary>
+        Stopped,
+
+        /// <summary>
+        /// Kinect is running
+        /// </summary>
+        Running
+    }
+}
--- src/Framework/Core/MyKinect.cs
+++ src/Framework/Core/MyKinect.cs
@@ -15,38 +15,6 @@ using System.Threading;
 namespace Kinect.Core
 {
     /// <summary>
-    /// Kinect states
-    /// </summary>
-    [Flags]
-    public enum KinectState
-    {
-        /// <summary>
-        /// Kinect context is open
-        /// </summary>
-        ContextOpen,
-
-        /// <summary>
-        /// Kinect is initializing
-        /// </summary>
-        Initializing,
-
-        /// <summary>
-        /// Kinect has failed
-        /// </summary>
-        Failed,
-
-        /// <summary>
-        /// Kinect is stopped
-        /// </summary>
-        Stopped,
-
-        /// <summary>
-        /// Kinect is running
-        /// </summary>
-        Running
-    }
-
-    /// <summary>
     /// Get all data from Kinect and control Kinect.
     /// </summary>
     public sealed class MyKinect : INotifyPropertyChanged
